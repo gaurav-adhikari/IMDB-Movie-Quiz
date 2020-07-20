6da7ea1b42212da8f96a38c6a4f2828ab6bc64f4
gaurav <gaurav.adhikari36@gmail.com>
2020-07-20 23:20:32 +0545

    db project setup

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -1,8 +1,12 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'btEjYQQonbKyuYL_FgFID9ywlldk_wgaEXmcMOoFpd4LJVM3UHy-AlmsOutey6Q3BAZR5-_dpg'
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///QuizSchema.db"
 
+db = SQLAlchemy(app)
+db.create_all()
 
 from quiz import routes
